24cb4537c638d92c6cf3ff03f75746e55d7945d7
Atai Ambus <45061342+TheAllSeeing@users.noreply.github.com>
2018-12-27 09:53:12 +0200

    First Commit

--- Character-Sheet/Character-Sheet/Program.cs
+++ Character-Sheet/Character-Sheet/Program.cs
@@ -16,7 +16,7 @@ namespace Character_Sheet
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Sheet());
         }
     }
 }
