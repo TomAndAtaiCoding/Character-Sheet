dfdc0a8d820666f53c22c235a556432bc92afdf1
OGPhoenixRider <45061386+OGPhoenixRider@users.noreply.github.com>
2019-02-07 14:38:23 +0200

    Revert "Armour class xml"

--- Character-Sheet/Character-Sheet/Sheet.cs
+++ Character-Sheet/Character-Sheet/Sheet.cs
@@ -22,14 +22,12 @@ namespace Character_Sheet
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Globals.HpPercenteBar = HPPercentBar; //Passes the current Hp Percentage Bar to globals, where Character will be able to use it.
+            Globals.HpPercenteBar = HPPercentBar;
         }
 
-        private void SetAcbutton_Click(object sender, EventArgs e) //Asks for confirmation and if confirmed changes all armour bonuses by their apropriate textBox
+        private void SetAcbutton_Click(object sender, EventArgs e)
         {
-            string l_ChangesText = ""; //This is the text that will be in the message asking for confirmation.
-            /*Creates an apropriate ArmourPart variable for every armour part, (taking new values from their proper textboxes), which are added to Globals.CurrentArmourParts in constructions,
-             together with their new values to Globals.NewArmourBonuses*/
+            string l_Armour = "";
             ArmourPart
                 HeadArmour = new ArmourPart("Head", Character.kHeadBaseArmour, character.HeadArmourBonus, int.Parse(HeadArmourBox.Text)),
                 ChestArmour = new ArmourPart("Chest", Character.kChestBaseArmour, character.ChestArmourBonus, int.Parse(ChestArmourBox.Text)),
@@ -39,19 +37,18 @@ namespace Character_Sheet
                 RightFootArmour = new ArmourPart("Right Foot", Character.kLegBaseArmour, character.RightFootArmourBonus, int.Parse(RLegArmourBox.Text)),
                 LeftFootArmour = new ArmourPart("Left Foot", Character.kLegBaseArmour, character.LeftFootArmourBonus, int.Parse(LLegArmourBox.Text));
 
-            l_ChangesText += Format("General Armour: {0} --> {1}", character.GetAC(), Globals.NewArmourBonuses.Sum()); //Adds the change to general armour to the message.
-            foreach (ArmourPart armourPart in Globals.CurrentArmourParts) //Iterates through the armour parts.
+            l_Armour += Format("General Armour: {0} --> {1}", character.GetAC(), Globals.NewArmourBonuses.Sum());
+            foreach (ArmourPart armourPart in Globals.CurrentArmourParts)
             {
-                if (armourPart.HasDifference()) //Checks if the value from the textbox is different from the current vaklue, and if not, adds the changes to the message.
+                if (armourPart.HasDifference())
                 {
-                    l_ChangesText += Format("{0} Armour Bonus: {1} --> {2}\n", armourPart.Part, armourPart.CurrentValue, armourPart.CurrentAimedValue);
-                    l_ChangesText += Format("Aimed {0} Armour: {1} --> {2}\n", armourPart.Part, armourPart.CurrentAimedValue, armourPart.NewAimedValue);
+                    l_Armour += Format("{0} Armour Bonus: {1} --> {2}\n", armourPart.Part, armourPart.CurrentValue, armourPart.CurrentAimedValue);
+                    l_Armour += Format("Aimed {0} Armour: {1} --> {2}\n", armourPart.Part, armourPart.CurrentAimedValue, armourPart.NewAimedValue);
                 }
             }
             
-            DialogResult l_Ans = MessageBox.Show(Format("Are you sure you want to change your Armour Class? to preview the changes:\n{0)", l_ChangesText), "Are You  Sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning); //The message asking for confirmation
-
-            if (l_Ans == DialogResult.Yes) //If the confirmation is given, set the value apropriatly.
+            DialogResult l_Ans = MessageBox.Show(Format("Are you sure you want to change your Armour Class? to preview the changes:\n{0)", l_Armour), "Are You  Sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if(l_Ans == DialogResult.Yes)
             {
                 character.HeadArmourBonus = HeadArmour.NewValue;
                 character.ChestArmourBonus = ChestArmour.NewValue;
